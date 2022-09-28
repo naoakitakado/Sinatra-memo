5ab2f709a15c2e15c30e35f692256be1679551f2
naoakitakado <naoakitakado666@gmail.com>
2022-09-28 14:56:47 +0900

    json形式で保存するように変更

--- app.rb
+++ app.rb
@@ -2,27 +2,21 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'cgi/escape'
-require 'dotenv/load'
+require 'json'
+require 'cgi'
 
-PATH = './views/memo/'
+PATH = 'public/memos.json'
+
+def get_memos(path)
+  File.open(path) { |file| JSON.parse(file.read) }
+end
+
+def set_memos(path, memos)
+  File.open(path, 'w') { |file| JSON.dump(memos, file) }
+end
 
 get '/' do
-  @memolist = []
-  @titles = []
-
-  Dir.foreach(PATH.to_s) do |item|
-    case item
-    when '.', '..', '.gitkeep'
-      nil
-    else
-      @memolist << item
-    end
-  end
-  @memolist.each do |filename|
-    File.open("#{PATH}#{filename}", 'r') { |f| @titles << f.gets.chomp }
-  end
-
+  @memos = get_memos(PATH)
   erb :top
 end
 
@@ -30,101 +24,51 @@ get '/memo/new' do
   erb :new
 end
 
-post '/memo' do
-  memolist = []
-  @titles = []
-  @title = params[:title]
-  @content = params[:content]
-
-  filename = ('A'..'Z').to_a.sample(11).join
-
-  File.open("./views/memo/#{filename}.txt", 'w') do |file|
-  file.puts @title.to_s
-  file.puts @content.to_s
-  end
-  Dir.foreach(PATH.to_s) do |item|
-    case item
-    when '.', '..', '.gitkeep'
-      nil
-    else
-      memolist << item
-    end
-  end
-
-  memolist.each do |item|
-    File.open("#{PATH}#{item}", 'r') { |f| @titles << f.gets.chomp }
-  end
-
-  redirect './'
-end
-
-get '/memo/:file' do
-  @filename = CGI.escapeHTML(params['file'].to_s)
-
-  memo = []
-  File.foreach("#{PATH}#{CGI.unescapeHTML(@filename)}") { |f| memo << f.chomp }
-  @title =  memo[0]
-  @contents = memo[1..]
+get '/memo/:id' do
+  memos = get_memos(PATH)
+  @title = memos[params[:id]]['title']
+  @content = memos[params[:id]]['content']
   erb :show
 end
 
-get '/memo/edit/:file' do
-  memolist = []
-  titles = []
-  @editfile = CGI.escapeHTML(params['file'])
-  @memo = []
+post '/memo/new' do
+  title = params[:title]
+  content = params[:content]
 
-  File.foreach("#{PATH}#{CGI.unescapeHTML(@editfile)}") { |f| @memo << f.chomp }
-  @title =  @memo[0]
-  contents = @memo[1..]
-  @content = contents.join("\n")
+  memos = get_memos(PATH)
+  id = if memos.to_s == '{}'
+         1
+       else
+         (memos.keys.map(&:to_i).max + 1).to_s
+       end
+  memos[id] = { 'title' => title, 'content' => content }
+  set_memos(PATH, memos)
+
+  redirect '/'
+end
+
+get '/memo/:id/edit' do
+  memos = get_memos(PATH)
+  @title = memos[params[:id]]['title']
+  @content = memos[params[:id]]['content']
   erb :edit
 end
 
-patch '/memo/edit/:file' do
-  editfile = CGI.escapeHTML(params['file'])
-  @title = CGI.escapeHTML(params[:title].to_s)
-  contents = CGI.escapeHTML(params[:content].to_s)
+patch '/memo/:id' do
+  title = params[:title]
+  content = params[:content]
 
-  File.open("./views/memo/#{filename}.txt", 'w') do |file|
-  file.puts @title.to_s
-  file.puts @content.to_s
-  end
+  memos = get_memos(PATH)
+  memos[params[:id]] = { 'title' => title, 'content' => content }
+  set_memos(PATH, memos)
 
-  memolist = []
-  @titles = []
-
-  Dir.foreach(PATH.to_s) do |item|
-    case item
-    when '.', '..', '.gitkeep'
-      nil
-    else
-      memolist << item
-    end
-  end
-  memolist.each do |filename|
-    File.open("#{PATH}#{filename}", 'r') { |f|  @titles << f.gets.chomp }
-  end
-
-
-  redirect './'
+  redirect "/memo/#{params[:id]}"
 end
 
-delete '/memo/:file' do
-  @memolist = []
-  @titles = []
+delete '/memo/:id' do
+  memos = get_memos(PATH)
+  memos.delete(params[:id])
+  set_memos(PATH, memos)
 
-  deletefile = CGI.escapeHTML(params['file'])
-  File.delete("#{PATH}#{CGI.unescapeHTML(deletefile)}")
-
-  Dir.foreach(PATH.to_s) do |item|
-    case item
-    when '.', '..'
-      nil
-    else
-      @memolist << item
-    end
-  end
-
-  redirect './'
+  redirect '/'
 end
